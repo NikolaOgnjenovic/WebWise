dfd7b37fa893adfccdaf70bca63dbd4295873d75
Damjan Vincic <damjanvincic2003@gmail.com>
2024-04-27 23:26:06 +0200

    Add user username to chat message serializer

--- api/src/apps/chat_messages/serializers.py
+++ api/src/apps/chat_messages/serializers.py
@@ -1,12 +1,14 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import ChatMessage
 
 
 class ChatMessageSerializer(ModelSerializer):
     class Meta:
         model = ChatMessage
-        fields = ('id', 'message', 'user', 'created_at')
-        read_only_fields = ('id', 'created_at')
-        # extra_kwargs = {
-        #     'user': {'read_only': True}
-        # }
+        fields = ('id', 'message', 'user', 'created_at', 'username')
+        read_only_fields = ('id', 'created_at', 'username')
+
+    username = SerializerMethodField('_get_user_username')
+
+    def _get_user_username(self, obj: ChatMessage):
+        return obj.user.username
