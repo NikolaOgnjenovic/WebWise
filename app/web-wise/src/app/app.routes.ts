ebe59f2113745ac03d3b0f182b167f3a1fab076f
mrmi <nikola.31023@gmail.com>
2024-04-27 13:37:46 +0200

    Navbar component in homepage, learn and contribute components

--- app/web-wise/src/app/app.routes.ts
+++ app/web-wise/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import {HomepageComponent} from "./homepage/homepage.component";
+import {LearnComponent} from "./learn/learn.component";
+import {ContributeComponent} from "./contribute/contribute.component";
 
 export const routes: Routes = [
-  { path: '', component: HomepageComponent }
+  { path: '', component: HomepageComponent },
+  { path: 'learn', component: LearnComponent },
+  { path: 'contribute', component: ContributeComponent }
 ];
