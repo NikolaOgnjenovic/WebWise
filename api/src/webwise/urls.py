592b64c51f1183145cad9bc76113322f1abf02dc
Damjan Vincic <damjanvincic2003@gmail.com>
2024-04-27 14:26:33 +0200

    Add initial endpoint

--- api/src/webwise/urls.py
+++ api/src/webwise/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import IndexApiView
+
+api_urlpatterns = [
+    path('', IndexApiView.as_view()),
+    path('admin/', admin.site.urls),
+]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/v1/', include(api_urlpatterns)),
 ]
