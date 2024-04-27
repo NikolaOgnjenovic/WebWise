826c7ef3c2d9e2e1b27f951796c297bad6fb834e
Damjan Vincic <damjanvincic2003@gmail.com>
2024-04-27 18:53:54 +0200

    Update entrypoint script for api and docker compose

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Wait for database
+echo "Waiting for PostgreSQL...\n" "info"
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$DB_HOSTNAME" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+    echo >&2 "Postgres is unavailable - sleeping\n" "info"
+    sleep 1
+done
+
 echo "Applying migrations..."
 python3 manage.py migrate
 
